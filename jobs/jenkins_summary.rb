48f3fd04157866d17f6c3d07a669296a70fbe40f
Andy Maes <andy@kabisa.nl>
2015-08-17 13:41:20 +0200

    remove failing spec test

--- jobs/jenkins_summary.rb
+++ jobs/jenkins_summary.rb
@@ -9,7 +9,7 @@ SCHEDULER.every '10s', first_in: 0 do
   jenkins = JenkinsClient.new(JENKINS_SUMMARY_HOST, JENKINS_SUMMARY_USERNAME, JENKINS_SUMMARY_TOKEN)
   jobs = jenkins.get_jobs(view: JENKINS_SUMMARY_VIEW)
 
-  failing_jobs = -> (j) { j['color'] == 'blue' }
+  failing_jobs = -> (j) { j['color'] == 'red' }
 
   red_jobs_count   = jobs.select(&failing_jobs).size
   green_jobs_count = jobs.reject(&failing_jobs).size
